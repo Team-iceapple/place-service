e545f3c61c1024a3a66b2553635ec7e85f9fa36d
Boyeon-Shin <anna007096@gmail.com>
2025-09-09 14:53:01 +0900

    test: controller test

--- src/main/java/iceapple/placeservice/web/PlaceController.java
+++ src/main/java/iceapple/placeservice/web/PlaceController.java
@@ -1,5 +1,7 @@
 package iceapple.placeservice.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import iceapple.placeservice.dto.response.PlaceListResponse;
 import iceapple.placeservice.dto.response.PlaceTimeCountResponse;
 import iceapple.placeservice.entity.Place;
@@ -7,6 +9,9 @@ import iceapple.placeservice.service.PlaceService;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,11 +26,26 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
-    @GetMapping
-    public ResponseEntity<PlaceListResponse> getPlaces() {
-        List<Place> places = placeService.findPlaces();
+//    @GetMapping
+//    public ResponseEntity<PlaceListResponse> getPlaces() {
+//        List<Place> places = placeService.findPlaces();
+//
+//        return ResponseEntity.ok(new PlaceListResponse(places));
+//    }
 
-        return ResponseEntity.ok(new PlaceListResponse(places));
+    @GetMapping
+    public ResponseEntity<PlaceListResponse> getPlaces() throws JsonProcessingException {
+        List<Place> places = placeService.findPlaces();
+        PlaceListResponse body = new PlaceListResponse(places);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        byte[] bodyBytes = objectMapper.writeValueAsBytes(body); // 실제 JSON 직렬화
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentLength(bodyBytes.length); // Content-Length 명시
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
     @GetMapping("/{place_id}")
